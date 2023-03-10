8bd57c489dcb492b50417604c8e6982444d9746f
bbarbis <baratebarbara2020@gmail.com>
2023-03-10 10:37:45 +0100

    Alap űrlap

---
+++ script.js
@@ -0,0 +1,8 @@
+window.addEventListener("load");
+function validateForm(){
+    let x = document.validTeszt.kNev.value;
+    if (x === ""){
+        alert("A mező kitöltése kötelező!");
+        return false;
+    }
+}
