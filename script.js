ef82eabf38688ad8612d4075513c2af85e31ecd7
bbarbis <baratebarbara2020@gmail.com>
2023-03-10 11:13:15 +0100

    jovo heten folytatjuk

--- script.js
+++ script.js
@@ -1,8 +1,17 @@
 window.addEventListener("load");
 function validateForm(){
-    let x = document.validTeszt.kNev.value;
+    let x = document.validTeszt.vNev.value;
+    let y = document.validTeszt.kNev.value;
     if (x === ""){
         alert("A mező kitöltése kötelező!");
+        return false;
+    }
+}
+function validateForm(){
+    let x = document.validTeszt.jelszo1.value;
+    let y = document.validTeszt.jelszo2.value;
+    if (x === ""){
+        alert("Nem egyforma a két jelszó");
         return false;
     }
 }
